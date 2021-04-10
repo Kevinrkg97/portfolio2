10eff6b8817d907507f0d36feb378a76082a697d
Kevinrkg97 <kevinrkg97@gmail.com>
2021-04-10 01:35:09 -0500

    slider

--- main.js
+++ main.js
@@ -27,3 +27,51 @@ document.addEventListener("keyup", e => {
     document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
 });
+
+
+/* SLIDER */
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".sliderItem");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btnLeft");
+const btnRight = document.querySelector("#btnRight");
+
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll(".sliderItem")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function Prev() {
+    let sliderSection = document.querySelectorAll(".sliderItem");
+    let sliderSectionLast = sliderSection[sliderSection.length -1]
+    slider.style.marginLeft = "0%";
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
+
+btnLeft.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 5000);
